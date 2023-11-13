05da141006bd833b628c77ba829c9031fab53a59
MinSeeok <124334092+MinSeeok@users.noreply.github.com>
2023-11-14 00:51:32 +0900

    '[deploy]'

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Vanilla Redux
 
 Learning Vanilla-Redux and React-Redux
+
+### Store는 data를 저장하는곳
+### CreateStore는 reducer를 요구함
+### Reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
+import { legacy_createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (state = 0) => {
+  console.log(state);
+  return state;
+};
 
-number.innerText = count;
+const countStore = legacy_createStore(countModifier);
 
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-}
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
