89d88093ddd6659c6181f424bf622a6f3e051190
MinSeeok <124334092+MinSeeok@users.noreply.github.com>
2023-11-14 01:01:14 +0900

    'commit'

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Learning Vanilla-Redux and React-Redux
 ### Store는 data를 저장하는곳
 ### CreateStore는 reducer를 요구함
 ### Reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨
+
+### Action: redux에서 FUCTION을 부를 때 쓰는 두 번째 parameter
+### Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  console.log(state);
-  return state;
+const countModifier = (count = 0, action) => {
+  if(action.type === "ADD"){
+    return count + 1;
+  } else if(action.type === "MINUS"){
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = legacy_createStore(countModifier);
+
+console.log(countStore.dispatch({type: "ADD"}));
+console.log(countStore.dispatch({type: "ADD"}));
+console.log(countStore.dispatch({type: "ADD"}));
+console.log(countStore.dispatch({type: "ADD"}));
+console.log(countStore.dispatch({type: "ADD"}));
+console.log(countStore.dispatch({type: "MINUS"}));
 
 console.log(countStore.getState());
